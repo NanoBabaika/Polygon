c2023c8b3b07549c7f94743db4447fa20562c545
Alexandr Skoromets <skoromets1990@mail.ru>
2024-10-25 15:59:44 +0300

    start work on form

--- poly_src/form_post.js
+++ poly_src/form_post.js
@@ -1,1 +1,23 @@
 let nameValue = document.getElementById('form_name');
+let phoneValue = document.getElementById('form_phone');
+let sendBtn = document.getElementById('submit_btn');
+let contactInfo = [];
+
+
+
+sendBtn.onclick = function (event) {
+    event.preventDefault();
+    console.log('клик по кнопке отправить')
+    getValue ();
+}
+ 
+
+function getValue () {
+    let inputValueName = document.getElementById('form_name').value;
+    let inputValuePhone = document.getElementById('form_phone').value;
+    let InfoValue = `Контакты клиента имя: ${inputValueName} контактный телефон ${inputValuePhone}`
+    contactInfo.push(InfoValue);
+    
+}
+
+console.log(contactInfo);
